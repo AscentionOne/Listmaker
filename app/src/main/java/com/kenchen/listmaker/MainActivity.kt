aeceddfb577f1fc81c8a2d19b25acdfb68672f09
KenChen <ken22885@gmail.com>
2022-09-06 16:18:19 +0800

    Add recycler view to content_main.xml

--- app/src/main/java/com/kenchen/listmaker/MainActivity.kt
+++ app/src/main/java/com/kenchen/listmaker/MainActivity.kt
@@ -2,13 +2,13 @@ package com.kenchen.listmaker
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import com.kenchen.listmaker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
